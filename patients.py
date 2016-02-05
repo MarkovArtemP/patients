ac89ffb7543647d57979fce323231156ebd5bc8d
MarkovArtemP <markov.artem.p@gmail.com>
2016-02-05 21:22:32 +0300

    Add horizontal lables into table

--- patients.py
+++ patients.py
@@ -1,25 +1,63 @@
 import sys
-from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from PyQt4.QtSql import *
 
-app = QApplication(sys.argv)
-table = QTableWidget()
-table.setWindowTitle("Patients")
+def getQuery(host, port, database, user, password):
+    db = QSqlDatabase.addDatabase("QMYSQL")
+    db.setHostName(host)
+    db.setPort(port)
+    db.setDatabaseName(database)
+    db.setUserName(user)
+    db.setPassword(password)
+    if db.open():
+        return QSqlQuery(db)
+    else:
+        return None
 
-db = QSqlDatabase.addDatabase("QMYSQL")
-db.setHostName("127.0.0.1")
-db.setPort(3306)
-db.setDatabaseName("patients")
-db.setUserName("root")
-db.setPassword("bd2crcv6")
-ok = db.open()
+def getTable(query):
+    table = QTableWidget()
+    table.setWindowTitle("Patients")
+    table.setRowCount(query.size())
+    table.setColumnCount(query.record().count() - 2)
+    record = q.record()
+    labels = ['ФИО']
+    for i in range(3, record.count()):
+        labels.append(record.fieldName(i))
+    table.setHorizontalHeaderLabels(labels)
+    index = 0
+    while query.next():
+        table.setItem(index, 0, QTableWidgetItem(query.value(0) + ' ' + query.value(1) + ' ' + query.value(2)))
+        table.setItem(index, 1, QTableWidgetItem(query.value(3)))
+        sex = "муж" if query.value(4) == 1 else "жен"
+        table.setItem(index, 2, QTableWidgetItem(sex))
+        for i in range(3, query.record().count() - 2):
+            table.setItem(index, i, QTableWidgetItem(query.value(i + 2)))
+        index += 1
+    table.resizeColumnsToContents()
+    return table
 
-query = QSqlQuery(db)
-query.exec_(
-        "SELECT Client.lastName, Client.firstName, Client.patrName, DATE_FORMAT(Client.birthDate, '%d.%m.%Y'), Client.sex, " +
-        "rbDocumentType.name, ClientDocument.serial, ClientDocument.number, DATE_FORMAT(ClientDocument.date, '%d.%m.%Y'), " +
-        "rbPolicyType.name, ClientPolicy.serial, ClientPolicy.number, DATE_FORMAT(ClientPolicy.begDate, '%d.%m.%Y'), DATE_FORMAT(ClientPolicy.endDate, '%d.%m.%Y'), ClientPolicy.name, ClientPolicy.note " +
+# TODO init params
+h = "127.0.0.1"
+p = 3306
+dbase = "patients"
+u = "root"
+passwd = "bd2crcv6"
+q = getQuery(h, p, dbase, u, passwd)
+q.exec_(
+        "SELECT Client.lastName, Client.firstName, Client.patrName, " +
+        "DATE_FORMAT(Client.birthDate, '%d.%m.%Y') AS 'Дата рождения', " +
+        "Client.sex AS 'Пол', " +
+        "rbDocumentType.name AS 'Тип документа', " +
+        "ClientDocument.serial AS 'Серия', " +
+        "ClientDocument.number AS 'Номер', " +
+        "DATE_FORMAT(ClientDocument.date, '%d.%m.%Y') AS 'Дата выдачи', " +
+        "rbPolicyType.name AS 'Тип полиса', " +
+        "ClientPolicy.serial AS 'Серия', " +
+        "ClientPolicy.number AS 'Номер', " +
+        "DATE_FORMAT(ClientPolicy.begDate, '%d.%m.%Y') AS 'Дата начала', " +
+        "DATE_FORMAT(ClientPolicy.endDate, '%d.%m.%Y') AS 'Дата окончания', " +
+        "ClientPolicy.name AS 'Name', " +
+        "ClientPolicy.note AS 'Note' " +
         "FROM Client " +
         "LEFT JOIN ClientDocument ON ClientDocument.client_id = Client.id " +
         "LEFT JOIN ClientPolicy ON ClientPolicy.client_id = Client.id " +
@@ -27,19 +65,7 @@ query.exec_(
         "LEFT JOIN rbPolicyType ON rbPolicyType.id = ClientPolicy.policyType_id " +
         ";")
 
-table.setColumnCount(query.record().count() - 2)
-table.setRowCount(query.size())
-
-index = 0
-while query.next():
-    table.setItem(index, 0, QTableWidgetItem(query.value(0) + ' ' + query.value(1) + ' ' + query.value(2)))
-    table.setItem(index, 1, QTableWidgetItem(query.value(3)))
-    sex = "муж" if query.value(4) == 1 else "жен"
-    table.setItem(index, 2, QTableWidgetItem(sex))
-    for i in range(3, query.record().count() - 2):
-        table.setItem(index, i, QTableWidgetItem(query.value(i + 2)))
-    index += 1
-
-table.show()
-
+app = QApplication(sys.argv)
+tabl = getTable(q)
+tabl.show()
 sys.exit(app.exec_())
