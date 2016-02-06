5aefafb8a667a465af7c59cfb829636e8f17c88b
MarkovArtemP <markov.artem.p@gmail.com>
2016-02-06 23:46:30 +0300

    aa

--- PatientsWindow.py
+++ PatientsWindow.py
@@ -10,56 +10,61 @@ class PatientsWindow(QWidget):
         self.showMaximized()
         self.setWindowTitle("Patients")
 
-        #CREATE MENU
+        # CREATE MENU
 
-        #create exit option
+        # create exit option
         exit = QAction('Exit', self)
         exit.setShortcut('Ctrl+Q')
         exit.setStatusTip('Exit application')
         self.connect(exit, SIGNAL('triggered()'), SLOT('close()'))
 
-        #create font dialog option
+        # create font dialog option
         font = QAction('Font', self)
         font.setStatusTip('Change font')
         self.connect(font, SIGNAL('triggered()'), self.showFontDialog)
 
-        #create menu bar
+        # create menu bar
         menubar = QMenuBar()
         file = menubar.addMenu('&File')
         file.addAction(exit)
         settings = menubar.addMenu('&Settings')
         settings.addAction(font)
 
-        #CREATE SEARCH AREA
+        # CREATE SEARCH AREA
 
-        #create text fields
+        searchVBox = QVBoxLayout()
+
+        # create top buttons
+
+        addButton = QPushButton("Add search field")
+        self.connect(self.addButton, SIGNAL('clicked()'), self.add)
+        removeAllButton = QPushButton("Remove all")
+        topButtonsHBox = QHBoxLayout()
+        topButtonsHBox.addWidget(addButton)
+        topButtonsHBox.addWidget(removeAllButton)
+        searchVBox.addLayout(topButtonsHBox)
+
+        # create search fields
         labels = header.copy()
         labels.__delitem__(0)
         labels.insert(0, 'Отчество')
         labels.insert(0, 'Имя')
         labels.insert(0, 'Фамилия')
 
-        qLabels = []
-        for i in labels:
-            qLabels.append(QLabel(i))
+        fieldsHBox = QHBoxLayout()
+        sField = SearchField(labels, fieldsHBox)
+        fieldsHBox.addLayout(sField)
+        searchVBox.addLayout(fieldsHBox)
 
-        linesEdit = []
-        for i in range(0, len(labels)):
-            linesEdit.append(QLineEdit())
-
-        searchHBox = QHBoxLayout()
-        for i in range(0, len(labels)):
-            searchHBox.addWidget(qLabels[i])
-            searchHBox.addWidget(linesEdit[i])
-
-        #Create buttons
+        # Create bottom buttons
         searchButton = QPushButton("Search")
-        clearButton = QPushButton("Clear")
-        buttonsHBox = QHBoxLayout()
-        buttonsHBox.addWidget(searchButton)
-        buttonsHBox.addWidget(clearButton)
+        clearButton = QPushButton("Clear all")
+        botButtonsHBox = QHBoxLayout()
+        botButtonsHBox.addWidget(searchButton)
+        botButtonsHBox.addWidget(clearButton)
+        searchVBox.addLayout(botButtonsHBox)
 
-        #CREATE TABLE
+        # CREATE TABLE
 
         table_model = PatientsTableModel(self, data_list, header)
         table_view = QTableView()
@@ -70,15 +75,15 @@ class PatientsWindow(QWidget):
         table_view.setSortingEnabled(True)
         self.table = table_view
 
-        #CREATE MAIN LAYOUT
-        
+        # CREATE MAIN LAYOUT
+
         vbox = QVBoxLayout()
         vbox.addWidget(menubar)
-        vbox.addLayout(searchHBox)
-        vbox.addLayout(buttonsHBox)
+        vbox.addLayout(searchVBox)
         vbox.addWidget(table_view)
 
         self.setLayout(vbox)
+
     def showFontDialog(self):
         font, ok = QFontDialog.getFont()
         if ok:
@@ -112,10 +117,32 @@ class PatientsTableModel(QAbstractTableModel):
         return None
 
     def sort(self, col, order):
-        """sort table by given column number col"""
         self.emit(SIGNAL("layoutAboutToBeChanged()"))
         self.data_list = sorted(self.data_list,
                                 key=operator.itemgetter(col))
         if order == Qt.DescendingOrder:
             self.data_list.reverse()
         self.emit(SIGNAL("layoutChanged()"))
+
+
+class SearchField(QHBoxLayout):
+    def __init__(self, cbItems, parent):
+        QHBoxLayout.__init__(self)
+        self.parent = parent
+        self.cb = QComboBox()
+        for i in cbItems:
+            self.cb.addItem(i)
+        self.addWidget(self.cb)
+
+        self.lineEdit = QLineEdit()
+        self.addWidget(self.lineEdit)
+
+        self.removeButton = QPushButton("Remove")
+        self.connect(self.removeButton, SIGNAL('clicked()'), self.delete)
+        self.addWidget(self.removeButton)
+
+    def delete(self):
+        self.cb.close()
+        self.lineEdit.close()
+        self.removeButton.close()
+        self.parent.removeItem(self)
