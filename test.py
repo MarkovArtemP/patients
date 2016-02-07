83ecb8048223222864f3cc113a2a793057d0cc1b
MarkovArtemP <markov.artem.p@gmail.com>
2016-02-07 20:22:32 +0300

    w

--- test.py
+++ test.py
@@ -1,30 +1,31 @@
 import sys
-from PyQt4 import QtGui,QtCore
-class LayoutTest(QtGui.QWidget):
-    def __init__(self):
-        super(LayoutTest, self).__init__()
-        self.horizontalLayout = QtGui.QVBoxLayout(self)
-        self.scrollArea = QtGui.QScrollArea(self)
-        self.scrollArea.setWidgetResizable(True)
-        self.scrollAreaWidgetContents = QtGui.QWidget()
-        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 380, 280))
-        self.horizontalLayout_2 = QtGui.QHBoxLayout(self.scrollAreaWidgetContents)
-        self.gridLayout = QtGui.QGridLayout()
-        self.horizontalLayout_2.addLayout(self.gridLayout)
-        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
-        self.add_button = QtGui.QPushButton("Add Items")
-        self.horizontalLayout.addWidget(self.scrollArea)
-        self.horizontalLayout.addWidget(self.add_button)
-        self.connect(self.add_button, QtCore.SIGNAL("clicked()"), self.addButtons)
-        self.setGeometry(300, 200, 400, 300)
+from PyQt4.QtGui import *
 
-    def addButtons(self):
-        for i in range(0, 50):
-            self.r_button = QtGui.QPushButton("Button %s " % i)
-            self.gridLayout.addWidget(self.r_button)
 
-app = QtGui.QApplication(sys.argv)
-ex = LayoutTest()
+class combodemo(QWidget):
+    def __init__(self, parent=None):
+        super(combodemo, self).__init__(parent)
+
+        layout = QHBoxLayout()
+        self.cb = QComboBox()
+        self.cb.addItem("C")
+        self.cb.addItem("C++")
+        self.cb.addItems(["Java", "C#", "Python"])
+        self.cb.currentIndexChanged.connect(self.selectionchange)
+
+        layout.addWidget(self.cb)
+        self.setLayout(layout)
+        self.setWindowTitle("combo box demo")
+
+    def selectionchange(self, i):
+        print("Items in the list are :")
+
+        for count in range(self.cb.count()):
+            print(self.cb.itemText(count))
+        print("Current index", i, "selection changed ", self.cb.currentText())
+
+
+app = QApplication(sys.argv)
+ex = combodemo()
 ex.show()
 sys.exit(app.exec_())
-
